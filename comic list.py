f1a2e494c4abe0d612105bebd63dad343df0e669
CireWire <djcire956@gmail.com>
2016-08-18 18:41:45 -0500

    Create comic list.py

---
+++ comic list.py
@@ -0,0 +1,9 @@
+#Feel free to change the code around and prove that "fanboy" wrong!
+
+marvel = ["Spider-Man", "Captain America", "Iron Man", "Batman"]
+
+for comic in marvel:
+	if comic == "Batman":
+		print "Batman is not a Marvel comic!"
+	else:
+		print "I own ", comic
